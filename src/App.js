56810849c4ce7d751ccd08787358661d16c1052b
박태현 <89737399+Hyountape00@users.noreply.github.com>
2023-12-03 23:37:22 +0900

    dev text change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          i had change the project contents
         </p>
         <a
           className="App-link"
