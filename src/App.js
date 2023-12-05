0352ebe93cf9d83a20a7abed45163c04206309f6
박태현 <89737399+Hyountape00@users.noreply.github.com>
2023-12-05 23:39:15 +0900

    css setting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import './css/reset.css';
+import './css/component.css';
+import './css/utility.css';
+import Header from './layout'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          i had change the project contents
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/layout.js
@@ -0,0 +1,9 @@
+function Header(){
+    return (
+        <header className="flex-box justify-content-center align-items-center">
+            React Component(Header)
+        </header>
+    )
+}
+
+export default Header;
