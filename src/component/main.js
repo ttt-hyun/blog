e351bbf37dcecede9d448c72deeb117cc79a31b5
박태현 <89737399+Hyountape00@users.noreply.github.com>
2023-12-07 23:41:10 +0900

    md 추가

--- src/component/main.js
+++ src/component/main.js
@@ -1,4 +1,25 @@
+import React, {useState, useEffect} from 'react';
+
+import MarkDown from 'markdown-to-jsx';
+
+import 'github-markdown-css';
+
+
 function Main(){
+    const file_name = 'test.md';
+    const [post, setPost] = useState('');
+
+    useEffect(() => {
+        import(`../markdown/${file_name}`)
+        .then(res => {
+            fetch(res.default)
+                .then(res => res.text())
+                .then(res => setPost(res))
+        })
+        .catch(err => console.log(err));
+        // .then(res => console.log(res))
+        // .catch(err => console.log(err)); 
+    })
     return (
         <div className="max-grid ml-auto mr-auto flex-box flex-column">
             <div className="row">
@@ -6,8 +27,10 @@ function Main(){
                     <div className="float-title">
                         <h2>나의 첫 Git블로그</h2>
                     </div>
-                    <div className="bsc-post">
-                        
+                    <div className="bsc-post markdown-body">
+                        <MarkDown>
+                            {post}
+                        </MarkDown>
                     </div>
                 </div>
             </div>
